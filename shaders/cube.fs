0b5461892049ecdb278b8e3c83715d2303afa523
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-12-29 07:19:35 -0700

    diffuse lighting

--- shaders/cube.fs
+++ shaders/cube.fs
@@ -1,13 +1,22 @@
 #version 330 core
+in vec3 normal;
+in vec3 fragment_position;
+
 out vec4 fragment_color;
 
 uniform vec3 object_color;
 uniform vec3 light_color;
+uniform vec3 light_position;
 
 void main() {
 	float ambient_strength = 0.1;
 	vec3 ambient = light_color * ambient_strength;
 
-	vec3 result = ambient * object_color;
+	vec3 normal_dir = normalize(normal);
+	vec3 light_dir = normalize(light_position - fragment_position);
+	float diff = max(dot(normal_dir, light_dir), 0.0);
+	vec3 diffuse = light_color * diff;
+
+	vec3 result = (ambient + diffuse) * object_color;
 	fragment_color = vec4(result, 1.0);
 }
--- shaders/cube.vs
+++ shaders/cube.vs
@@ -1,5 +1,9 @@
 #version 330 core
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 a_normal;
+
+out vec3 normal;
+out vec3 frag_position;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -7,4 +11,6 @@ uniform mat4 projection;
 
 void main() {
 	gl_Position = projection * view * model * vec4(position, 1.0);
+	frag_position = vec3(model * vec4(frag_position, 1.0));
+	normal = a_normal;
 }
