eb7f83a0ea08a4ab0b1ae5a5ef1c7bceecc76c70
Joshua Kellogg <135467319+jkellogg01@users.noreply.github.com>
2024-12-29 13:57:48 -0700

    lighting - materials

--- shaders/cube.fs
+++ shaders/cube.fs
@@ -1,29 +1,45 @@
 #version 330 core
+
+struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+struct Light {
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 in vec3 normal;
 in vec3 fragment_position;
 
 out vec4 fragment_color;
 
-uniform vec3 object_color;
-uniform vec3 light_color;
-uniform vec3 light_position;
 uniform vec3 view_position;
+uniform Material material;
+uniform Light light;
 
 void main() {
-	float ambient_strength = 0.1;
-	vec3 ambient = light_color * ambient_strength;
+	// ambient
+	vec3 ambient = light.ambient * material.ambient;
 
+	// diffuse
 	vec3 normal_dir = normalize(normal);
-	vec3 light_dir = normalize(light_position - fragment_position);
+	vec3 light_dir = normalize(light.position - fragment_position);
 	float diff = max(dot(normal_dir, light_dir), 0.0);
-	vec3 diffuse = light_color * diff;
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
-	float specular_strength = 0.5;
+	// specular
 	vec3 view_dir = normalize(view_position - fragment_position);
 	vec3 reflect_dir = reflect(-light_dir, normal_dir);
-	float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
-	vec3 specular = specular_strength * spec * light_color;
+	float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
+	vec3 specular = light.specular * (diff * material.specular);
 
-	vec3 result = (ambient + diffuse + specular) * object_color;
+	vec3 result = ambient + diffuse + specular;
 	fragment_color = vec4(result, 1.0);
 }
